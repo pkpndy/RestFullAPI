08cdd998afe77261e5e53c885a7337df2b7ac7ba
Prakash Pandey <pkpndy@gmail.com>
2024-03-19 15:23:46 +0530

    added mongoDB for database

--- index.js
+++ index.js
@@ -1,75 +1,99 @@
 const express = require('express');
 const fs = require('fs');
-const users =   require('./MOCK_DATA.json');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 8000;
 
+//connection
+mongoose.connect('mongodb://127.0.0.1:27017/trialdb')
+.then(() => console.log("mongoDB connected"))
+.catch((err) => console.log(err));
+
+//schema
+const userSchema = new mongoose.Schema({
+    firstName:{
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    gender:{
+        type: String,
+    },
+    jobTitle:{
+        type: String
+    },
+},
+{timestamps: true}
+);
+
+//Model creation
+const User = mongoose.model("user", userSchema);
+
 app.use(express.urlencoded({extended: false}));
 
 app.route("/api/users/:id")
-.get((req, res) => {
-    const userID = Number(req.params.id);
-    if(userID > users.length){
-        return res.json({err: "id greater than users list"});
-    }
-    const user = users.find((user) => user.id === userID);
-    return res.json(user);
+.get(async(req, res) => {
+    const user = await User.findById(req.params.id);
+    if(!user) return res.status(404).json({msg : "user not found"});
+    return res.status(200).json(user);
 })
-.patch((req, res) => {
-    const userID = Number(req.params.id);
-    if(userID > users.length){
-        return res.json({err: "id greater than users list"});
-    }
-    const userIndex = users.findIndex(user => user.id === userID);
-    const newData = req.body;
-    Object.assign(users[userIndex], newData);
-    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-        return res.json({status: "patchSuccess", id: userIndex+1 });
+.patch(async(req, res) => {
+    const body = req.body;
+    console.log(body);
+    console.log(req.params.id);
+    const user = await User.findByIdAndUpdate(req.params.id, {
+        firstName: body.first_name,
+        lastName : body.last_name,
+        email : body.email,
+        gender : body.gender,
+        jobTitle: body.job_title,
     });
+    if(!user) return res.status(404).json({msg : "user not found"});
+    return res.status(201).json({status: "patchSuccess"});
+
 })
-.delete((req, res) => {
-    const userID = Number(req.params.id);
-    if(userID > users.length){
-        return res.json({err: "id greater than users list"});
-    }
-    const lastUserBefore = users.length;
-    const userIndex = users.findIndex(user => user.id === userID);
-    users.splice(userIndex, 1)[0];
-    const lastUserAfter = users.length;
-    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-        return res.json({status: "userDeleted", usersBefore: lastUserBefore, usersAfter: lastUserAfter});
-    })
+.delete(async(req, res) => {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if(!user) return res.status(404).json({msg : "user not found"});
+    return res.status(200).json({status: "userDeleted"});
 })
 
 app.route("/api/users")
-.post((req, res) => {
+.post(async(req, res) => {
     const body = req.body;
-    console.log(body);
-    const lastUserID = Number(users.length + 1);
-    users.push({
-        "id" : lastUserID,
-        "first_name": body.first_name,
-        "last_name" : body.last_name,
-        "email" : body.email,
-        "gender" : body.gender,
-        "job_title" : body.job_title
+    if(!body || !body.first_name || !body.last_name || !body.email || !body.gender || !body.job_title){
+        return res.status(404).json({msg: "All fields are required..."});
+    }
+    const result = await User.create({
+        firstName: body.first_name,
+        lastName : body.last_name,
+        email : body.email,
+        gender : body.gender,
+        jobTitle: body.job_title,
     });
-    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-        return res.json({status: "postSuccess", id: lastUserID});
-    });
-}).get((req, res) => {
-    return res.json(users);
+    return res.status(201).json({msg : "success"});
+}).get(async(req, res) => {
+    const users = await User.find({});
+    return res.status(200).json(users);
 })
 
-app.get("/users", (req, res) => {
+app.get("/users", async(req, res) => {
+    const users = await User.find({});
     const html =
     `
     <ul>
-        ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+        ${users.map((user) => `<li>${user.firstName}</li>`).join("")}
     </ul>
     `
-    res.send(html);
+    res.status(200).send(html);
 })
 
 app.listen(PORT, () => {
